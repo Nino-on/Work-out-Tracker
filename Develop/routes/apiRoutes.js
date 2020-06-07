a49945d1c6f56d15c6bb957ac2bb7ac77920bb52
Ren <raulnino24@gmail.com>
2020-06-06 20:20:25 -0400

    heroku

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -11,6 +11,15 @@ module.exports = function (app) {
                 res.json(err)
             })
     });
+    app.get("/api/workouts/range",function(req,res){  
+        Workout.find()
+        .then(data =>{  
+            res.json(data)
+        })
+        .catch(err => { 
+            res.json(err)
+        })
+    });
 
     app.post("/api/workouts", function (req, res) {
         Workout.create({})
@@ -19,6 +28,13 @@ module.exports = function (app) {
                 console.log("err", err)
                 res.json(err)
             })
+    });
+    app.post("/api/workouts/range",function (req,res){    
+        Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
     });
 
     app.put("/api/workouts/:id", ({ body, params }, res) => {
