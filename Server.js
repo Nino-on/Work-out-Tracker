2f2d7a08d59a276f01df7827cfe74cab3ee39c65
Ren <raulnino24@gmail.com>
2020-06-08 15:48:03 -0400

    test

--- Server.js
+++ Server.js
@@ -1,29 +1,26 @@
-// Dependencies
 const express = require("express");
-const morgan = require("morgan");
+const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
-
-// Setting up Express App
 const app = express();
-const PORT = process.env.PORT || 3002;
 
-app.use(morgan("dev"));
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
 app.use(express.static('public'));
 
-// db mongo
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI);
 
 // Creating Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// Starts the server to begin listening
-app.listen(PORT, function(){
-    console.log(`App listening on Port ${PORT}!`);
-})
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
