db99627a9dd6a49427874666e3172f28d6b14e06
Ren <raulnino24@gmail.com>
2020-06-08 15:38:32 -0400

    push again

--- Server.js
+++ Server.js
@@ -15,11 +15,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // db mongo
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
+mongoose.connect(MONGODB_URI);
 
 // Creating Routes
 require("./routes/apiRoutes")(app);
